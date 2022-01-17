c09bb5248057d2a14bfe13d257c6fdf4ed736d43
Rick Moynihan <rick.m@swirrl.com>
2022-01-17 13:09:32 +0000

    Rename image from cimg/clojure to swirrl/cimg-clojure
    NOTE: this is a hack, I didn't change the scripts in the submodule to
    generate this.

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.10/Dockerfile -t cimg/clojure:1.10.3  -t cimg/clojure:1.10 .
-docker build --file 1.10/node/Dockerfile -t cimg/clojure:1.10.3-node  -t cimg/clojure:1.10-node .
-docker build --file 1.10/browsers/Dockerfile -t cimg/clojure:1.10.3-browsers  -t cimg/clojure:1.10-browsers .
+docker build --file 1.10/Dockerfile -t swirrl/cimg-clojure:1.10.3  -t swirrl/cimg-clojure:1.10 .
+docker build --file 1.10/node/Dockerfile -t swirrl/cimg-clojure:1.10.3-node  -t swirrl/cimg-clojure:1.10-node .
+docker build --file 1.10/browsers/Dockerfile -t swirrl/cimg-clojure:1.10.3-browsers  -t swirrl/cimg-clojure:1.10-browsers .
